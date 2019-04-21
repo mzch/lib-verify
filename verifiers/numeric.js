fd9f9f72c31006d6b02860650ad4b1bb4f439f50
Alexander Craggs <popey@debenclipper.com>
2019-04-22 00:16:15 +0100

    Update numeric verification

--- verifiers/numeric.js
+++ verifiers/numeric.js
@@ -1,35 +1,47 @@
 function isNumber() {
-	return field => isNan(field) ? `{field} is not a number but should be a number` : undefined
+	return field => isNan(field) ? `{field} expects a number but found '${field}'` : undefined
 }
 
 function maxValue(val) {
-	return field => field > val ? `{field} field is too high.  '${field}' is ${field - val} too high` : undefined
+	return field => field > val ? `{field} expects a number below ${val} but found ${field}.  This is ${field - val} too high.` : undefined
 }
 
 function minValue(val) {
-	return field => field < val ? `{field} field is too low.  '${field} is ${val - field} too low` : undefined
+	return field => field < val ? `{field} expects a number above ${val} but found ${field}.  This is ${val - field} too low.` : undefined
 }
 
 function equals(val) {
-	return field => val ? undefined : `{field} is '${field}'' but should equal '${val}'`
+	return field => field === val ? undefined : `{field} should equal '${field}', but found '${field}'`
 }
 
 function range(min, max) {
-	return field => (field > val) || (field < val) ? `{field} is '${field}' but should be between '${min}' and '${max}'` : undefined
+	return field => (field >= max) || (field < min) ? `{field} expects a number between '${min}' and '${max}' but found '${field}'` : undefined
 }
 
 function isEven() {
-	return field => field % 2 == 0 ? undefined : `{field} should be even`
+	return field => field % 2 === 0 ? undefined : `{field} should be even, but was odd.`
 }
 
 function isOdd() {
-	return field => field % 2 == 1 ? undefined : `{field} should be odd`
+	return field => field % 2 === 1 ? undefined : `{field} should be odd, but was even.`
 }
 
 function isMultiple(mul, remainder) {
-	return field => field % mul == remainder ? undefined : `{field} should be a multiple of '${mul}'${remainder != 0 ? ` with a remainder of '${remainder}'` : ''}`
+	return field => field % mul === remainder ? undefined : `{field} should be a multiple of '${mul}'${remainder != 0 ? ` with a remainder of '${remainder}'` : ''} but was not.`
 }
 
 function isInt() {
-	return field => String(field).indexOf(".") == -1 ? undefined : `{field} must be an integer`
+	return field => String(field).indexOf(".") == -1 ? undefined : `{field} must be an integer but found '${field}'`
+}
+
+function isPositive(andZero = true) {
+	return field => field > 0 || (field === 0 && andZero) ? undefined : `{field} expected a positive number but got '${field}'`
+}
+
+function isNegative(andZero = true) {
+	return field => field < 0 || (field === 0 && andZero) ? undefined : `{field} expected a negative number but got '${field}'`
+}
+
+module.exports = {
+	isNumber, maxValue, minValue, equals, range, isEven, isOdd, isMultiple, isInt, isPositive, isNegative
 }
